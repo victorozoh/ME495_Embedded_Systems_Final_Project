fd9ec888ad65d51e7bced0fae9c6f869e8b1015c
pswiss <petrasswissler2022@u.northwestern.edu>
2018-12-05 12:57:37 -0600

    fixed update

--- src/findSerialPort.py
+++ src/findSerialPort.py
@@ -9,7 +9,7 @@ def runMe():
 	# find Serial ports
 	sPorts = [comport.device for comport in serial.tools.list_ports.comports()]
 	print(sPorts)
-	
+
 	# Open the com port
 	ser = serial.Serial()
 	ser.baudrate = 115200
@@ -17,14 +17,6 @@ def runMe():
 	ser.open()
 	while 1:
 		print ser.read(100)
-=======
-    ser = serial.Serial(serial_ports()[0],115200)
-    ser.close()
-    ser.open()
-    while 1:
-        print(ser.readline())
->>>>>>> 4b60c140564df152607861e39a2a8b5b2655cf43
-
 
 if __name__ == '__main__':
     runMe()
