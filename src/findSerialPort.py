ef5b666a8cdedd791dfaf971876cb1e42672f384
pswiss <petrasswissler2022@u.northwestern.edu>
2018-12-04 19:28:36 -0600

    Added file that should theoretically allow see all serial ports and read one of them...

---
+++ src/findSerialPort.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+import glob
+import serial
+
+""
+def serial_ports():
+	#Source https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+def runMe():
+    ser = serial.Serial('/dev/tty.usbserial',115200)
+    while 1:
+        print ser.readLine()
+
+
+if __name__ == '__main__':
+    print(serial_ports())
+    runMe()
