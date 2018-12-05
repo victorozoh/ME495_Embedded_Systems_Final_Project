85a484b8aa0ccab43c64f2f75df0f7c1995aba4a
pswiss <petrasswissler2022@u.northwestern.edu>
2018-12-05 12:54:46 -0600

    Updated serial echo python script

--- src/findSerialPort.py
+++ src/findSerialPort.py
@@ -2,43 +2,21 @@
 import sys
 import glob
 import serial
-
-""
-def serial_ports():
-	#Source https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+import serial.tools.list_ports
 
 def runMe():
-    ser = serial.Serial('/dev/tty.usbserial',115200)
-    while 1:
-        print ser.readLine()
+	# find Serial ports
+	sPorts = [comport.device for comport in serial.tools.list_ports.comports()]
+	print(sPorts)
+	
+	# Open the com port
+	ser = serial.Serial()
+	ser.baudrate = 115200
+	ser.port = sPorts[0]
+	ser.open()
+	while 1:
+		print ser.read(100)
 
 
 if __name__ == '__main__':
-    print(serial_ports())
     runMe()
